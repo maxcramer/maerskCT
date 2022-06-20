82b4a907013ac36d59514b4773f1bc4174bc6d3f
maxcramer <maximiliancramer@yahoo.com>
2022-06-20 12:50:15 +0100

    setting up tilesArr

---
+++ script.js
@@ -0,0 +1,13 @@
+var shuffSortObj = function shuffleAndSort() {
+  const tilesArr = [
+    { text: 1, class: "colorGrp1" },
+    { text: 2, class: "colorGrp2" },
+    { text: 3, class: "colorGrp3" },
+    { text: 4, class: "colorGrp2" },
+    { text: 5, class: "colorGrp3" },
+    { text: 6, class: "colorGrp4" },
+    { text: 7, class: "colorGrp4" },
+    { text: 8, class: "colorGrp1" },
+    { text: 9, class: "colorGrp3" },
+  ];
+};
